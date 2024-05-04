36a00b6f2956765c6ed92e265dc4f7628e0842f8
Dheeraj jha <dheerajjha834@gmail.com>
2024-05-05 03:46:21 +0530

    icon update

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,32 +1,43 @@
 import reactLogo from "../assets/youtube.svg";
 import Menu from "../assets/hamburger-menu.svg";
 import Search from "../assets/magnifying.svg";
+import Mic from "../assets/microphone.svg";
 
 const Head = () => {
   return (
-    <div className="h-[60px] bg-white sticky flex">
-      <div className="h-full flex items-center mx-4 relative">
-        <img src={Menu} className="w-7  object-cover" alt="youtube logo " />
-        <img
-          src={reactLogo}
-          className="min-w-24 h-full object-cover mx-2"
-          alt="youtube logo"
-        />
-        <span className="text-xs text-slate-400 absolute top-2.5 -right-1.5 ">
-          IN
-        </span>
+    <nav className="h-[60px] bg-white sticky flex w-full items-center px-[2.5vw]">
+      <div className="h-full flex items-center  relative">
+        <button className="p-2 rounded-full hover:bg-slate-300">
+          <img src={Menu} className="w-7  object-cover" alt="youtube logo " />
+        </button>
+        <a href="#" className=" min-w-24 h-full mx-3">
+          <img
+            src={reactLogo}
+            className="w-full h-full object-cover "
+            alt="youtube logo"
+          />
+          <span className="text-xs text-slate-400 absolute top-2 right-0 ">
+            IN
+          </span>
+        </a>
       </div>
-      <div className="flex h-full  items-center ">
+      <div className="flex relative max-w-[500px] w-1/2 items-center h-11 mx-6">
         <input
           type="text"
           placeholder="Search"
-          className="h-11 max-w-96 w-full my-auto border border-gray-300 rounded-l-3xl pl-3"
+          className="w-[85%] h-full border border-[#dbdbdb] rounded-l-3xl px-5 text-base capitalize"
         />
-        <button className="px-2 border border-gray-300 rounded-r-3xl h-11">
-          <img src={Search} alt="" className="w-8 h-11 " />
+        <button className="w-[15%] h-full border border-[#dbdbdb] rounded-r-3xl px-2 border-l-0">
+          <img src={Search} alt="search icon" className="w-8  " />
+        </button>
+        <button className="h-9 px-2 rounded-full justify-center bg-slate-200 hover:bg-slate-300  ml-4">
+          <img src={Mic} alt="Voice Aasistant" className="h-6  " />
         </button>
       </div>
-    </div>
+      <div className="h-9 flex ml-auto items-center">
+        <button></button>
+      </div>
+    </nav>
   );
 };
 
