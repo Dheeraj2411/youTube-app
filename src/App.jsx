e1b6d5b23ce74514c794acc0a7e5a98c61dd2b45
Dheeraj jha <dheerajjha834@gmail.com>
2024-05-04 23:47:50 +0530

    cmmt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/youtube.svg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="youtube logo" alt="youtube logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
