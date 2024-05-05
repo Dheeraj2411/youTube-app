77fd0400341d0f8b9d136cbcc45871eabd20686b
Dheeraj jha <dheerajjha834@gmail.com>
2024-05-05 13:00:31 +0530

    Head complete

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,41 +1,56 @@
 import reactLogo from "../assets/youtube.svg";
 import Menu from "../assets/hamburger-menu.svg";
 import Search from "../assets/magnifying.svg";
-import Mic from "../assets/microphone.svg";
+import Mic from "../assets/microphone-10.svg";
+import VideoAdd from "../assets/video-add.svg";
+import Bell from "../assets/bell.svg";
+import User from "../assets/user-circle.svg";
 
 const Head = () => {
   return (
-    <nav className="h-[60px] bg-white sticky flex w-full items-center px-[2.5vw]">
-      <div className="h-full flex items-center  relative">
-        <button className="p-2 rounded-full hover:bg-slate-300">
+    <nav className="h-[56px] bg-white fixed flex  items-center px-[2vw] justify-between w-full">
+      <div className="h-full  flex items-center  relative mr-3">
+        <button className="w-10 p-2 rounded-full hover:bg-slate-300 ">
           <img src={Menu} className="w-7  object-cover" alt="youtube logo " />
         </button>
-        <a href="#" className=" min-w-24 h-full mx-3">
+        <a href="#" className="relative w-24 h-full mx-3">
           <img
             src={reactLogo}
             className="w-full h-full object-cover "
             alt="youtube logo"
           />
-          <span className="text-xs text-slate-400 absolute top-2 right-0 ">
+          <span className="text-xs text-slate-400 absolute top-2 -right-2">
             IN
           </span>
         </a>
       </div>
-      <div className="flex relative max-w-[500px] w-1/2 items-center h-11 mx-6">
+      <div className="flex relative  w-1/2 max-w-[650px] items-center justify-center h-11 mx-10 align-middle ml-auto ">
         <input
           type="text"
           placeholder="Search"
-          className="w-[85%] h-full border border-[#dbdbdb] rounded-l-3xl px-5 text-base capitalize"
+          className="w-[calc(100%-100px)] h-full border border-[#dbdbdb] rounded-l-3xl px-5 text-base capitalize"
         />
-        <button className="w-[15%] h-full border border-[#dbdbdb] rounded-r-3xl px-2 border-l-0">
-          <img src={Search} alt="search icon" className="w-8  " />
+        <button className="w-16 h-full border border-[#dbdbdb] rounded-r-3xl px-2 border-l-0 bg-slate-100">
+          <img src={Search} alt="search icon" className="w-8 mx-auto " />
         </button>
-        <button className="h-9 px-2 rounded-full justify-center bg-slate-200 hover:bg-slate-300  ml-4">
-          <img src={Mic} alt="Voice Aasistant" className="h-6  " />
+        <button className="h-9 w-9   rounded-full  bg-slate-200 hover:bg-slate-300 ml-3 px-2">
+          <img
+            src={Mic}
+            alt="Voice Aasistant"
+            className="   h-full w-5 mx-auto"
+          />
         </button>
       </div>
-      <div className="h-9 flex ml-auto items-center">
-        <button></button>
+      <div className="h-9   flex ml-auto  items-center justify-end">
+        <button className="h-full w-9 mr-5 p-1.5  hover:bg-slate-300 rounded-full ">
+          <img src={VideoAdd} alt="video Add" className="h-full w-full " />
+        </button>
+        <button className="h-full w-9 mr-5 p-1.5  hover:bg-slate-300 rounded-full ">
+          <img src={Bell} alt="video Add" className="h-full w-full" />
+        </button>
+        <button className="h-full w-9 mr-5 p-1  hover:bg-slate-300 rounded-full ">
+          <img src={User} alt="video Add" className="h-full w-full" />
+        </button>
       </div>
     </nav>
   );
